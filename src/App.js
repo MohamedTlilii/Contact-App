dee709a3a86656fd88636cacc10e0058f7d6ad28
NAUT5 <Mohamedtlili1995@hotmail.com>
2023-12-13 12:54:32 +0100

    almost-done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div >
-      
+    <div>
+      <NavBar />
+      <Home />
     </div>
   );
 }
