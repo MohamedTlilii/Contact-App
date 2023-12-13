202487512cada457244fcb726ea5117325833167
NAUT5 <Mohamedtlili1995@hotmail.com>
2023-12-13 12:46:19 +0100

    almost-done

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import { createStore } from "redux";
+import rootReducer from "./reducers";
 
-function store() {
-  return (
-    <div>store</div>
-  )
-}
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-export default store
+export default store;
